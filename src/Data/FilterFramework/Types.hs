cb32eec90142ef22f3df7d3fd7afdfe9263ed7b2
John Haynes <jhaynes@funnelback.com>
2016-09-05 12:28:00 +1000

    added manyToMany filter

--- src/Data/FilterFramework/Types.hs
+++ src/Data/FilterFramework/Types.hs
@@ -4,15 +4,15 @@ import Control.Monad.Trans.Resource     (ResourceT)
 import Data.ByteString                  (ByteString)
 import Data.Conduit                     (Conduit)
 
-newtype Key = Key ByteString deriving Show
+newtype Key = Key ByteString deriving (Eq, Ord, Show)
 
-newtype Value = Value ByteString deriving Show
+newtype Value = Value ByteString deriving (Eq, Ord, Show)
 
-data Metadatum = Metadatum Key Value deriving Show
+data Metadatum = Metadatum Key Value deriving (Eq, Ord, Show)
 
-newtype Pristine = Pristine ByteString deriving Show
+newtype Pristine = Pristine ByteString deriving (Eq, Ord, Show)
 
-newtype Current = Current ByteString deriving Show
+newtype Current = Current ByteString deriving (Eq, Ord, Show)
 
 data FilterDocument = FilterDocument
                     { getMeta :: [Metadatum]
